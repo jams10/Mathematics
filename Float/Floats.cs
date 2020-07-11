d878cdbc99f277c88e3d1b3601cbc0a66c247f05
jams10 <jamin3710@gmail.com>
2020-07-11 11:39:38 +0000

    floats

---
+++ Float/Floats.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Floats
+{
+  public class ExponentBit
+  {
+    public static void SpecialBitProcess 
+    {
+      float denominator = 1.0f; // any number not 0
+      float numerator = 0.0f; 
+
+      float result = denominator / numerator;
+
+      // 1 Exponent Bit : 0xFF, Mantissa Bit : 0X00
+      Console.WriteLine($"result : {result}"); // result : Infinity
+
+      // 2 Exponent Bit : 0xFF, Mantissa Bit : not 0X00
+      float result2 = result / result; // infinity / infinity
+      Console.WriteLine($"result : {result2}"); // result : NaN(Not a Number)
+
+      // 3 Exponent Bit : 0xFF, Mantissa Bit : not 0X00
+      denominator = 0.0f;
+      float result3 = denominator / numerator; // 0 / 0
+      Console.WriteLine($"result : {result3}"); // result : NaN(Not a Number)
+    }
+  }
+}
--- main.cs
+++ main.cs
@@ -1,18 +1,8 @@
 using System;
-using Unsigned_Integer;
-using Signed_Integer;
-using UTF8;
+using Floats;
 
 class MainClass {
   public static void Main (string[] args) {
-    Addition_Overflow.test1();
-    Addition_Overflow.test2();
-    Addition_Overflow.test3();
-    
-    Two_Complement.Express_Negative_Intger();
-    Two_Complement.Negative_Signed_Integer();
-    Two_Complement.Signed_Integer_Addition_Overflow();
-
-    Console.WriteLine(utf8.Utf16_To_Utf8("한글"));
+    ExponentBit.SpecialBitProcess();
   }
 }
