734f884c2bf7058650fce925d98c192476564027
jams10 <jamin3710@gmail.com>
2020-08-18 00:44:41 +0000

    Solved GetNormal function error in Vector2D class

--- Vector/Vector2D.cpp
+++ Vector/Vector2D.cpp
@@ -1,14 +1,14 @@
 #include "Vector2D.h"
 #include <cmath>
 
-Vector2D::Vector2D( const int& x_in, const int& y_in )
+Vector2D::Vector2D(const int& x_in, const int& y_in)
 	:
 	x(x_in),
 	y(y_in)
 {
 }
 
-Vector2D::Vector2D( const Vector2D& ref )
+Vector2D::Vector2D(const Vector2D& ref)
 {
 	x = ref.x;
 	y = ref.y;
@@ -18,29 +18,29 @@ Vector2D::~Vector2D()
 {
 }
 
-Vector2D Vector2D::operator+( const Vector2D& rhs )
+Vector2D Vector2D::operator+(const Vector2D& rhs)
 {
-	return Vector2D( x + rhs.x, y + rhs.y );
+	return Vector2D(x + rhs.x, y + rhs.y);
 }
 
-Vector2D Vector2D::operator-( const Vector2D& rhs )
+Vector2D Vector2D::operator-(const Vector2D& rhs)
 {
-	return Vector2D( x - rhs.x, y - rhs.y );
+	return Vector2D(x - rhs.x, y - rhs.y);
 }
 
-Vector2D Vector2D::operator*( float rhs )
+Vector2D Vector2D::operator*(float rhs)
 {
-	return Vector2D( x * rhs, y * rhs);
+	return Vector2D(x * rhs, y * rhs);
 }
 
-Vector2D& Vector2D::operator=( const Vector2D& rhs )
+Vector2D& Vector2D::operator=(const Vector2D& rhs)
 {
-	if( this != &rhs )
+	if (this != &rhs)
 	{
 		this->x = rhs.x;
 		this->y = rhs.y;
 	}
-	
+
 	return *this;
 }
 
@@ -50,15 +50,23 @@ float Vector2D::GetLength()
 	float length = 0.0f;
 	length = x * x + y * y;
 
-	return sqrt( length );
+	return sqrt(length);
 }
 
-Vector2D& Vector2D::GetNormal()
+Vector2D Vector2D::GetNormal()
 {
 	const float len = GetLength();
-	if( len != 0.0f )
+	if (len != 0.0f)
 	{
 		return *this * (1.0f / len);
 	}
 	return *this;
 }
+
+/*
+<비 const에 대한 C ++ 초기 값은 lvalue 여야합니다.>
+
+https://www.it-swarm.dev/ko/c%2B%2B/%EB%B9%84-const%EC%97%90-%EB%8C%80%ED%95%9C-c-%EC%B4%88%EA%B8%B0-%EA%B0%92%EC%9D%80-lvalue-%EC%97%AC%EC%95%BC%ED%95%A9%EB%8B%88%EB%8B%A4/1040965278/
+
+https://thrillfighter.tistory.com/351
+*/
--- Vector/Vector2D.h
+++ Vector/Vector2D.h
@@ -4,15 +4,15 @@ class Vector2D
 {
 public:
 	Vector2D() = default;
-	Vector2D( const int& x_in, const int& y_in );
-	Vector2D( const Vector2D& ref );
+	Vector2D(const int& x_in, const int& y_in);
+	Vector2D(const Vector2D& ref);
 	~Vector2D();
-	Vector2D operator+( const Vector2D& rhs );
-	Vector2D operator-( const Vector2D& rhs );
-	Vector2D operator*( float rhs );
-	Vector2D& operator=( const Vector2D& rhs );
+	Vector2D operator+(const Vector2D& rhs);
+	Vector2D operator-(const Vector2D& rhs);
+	Vector2D operator*(float rhs);
+	Vector2D& operator=(const Vector2D& rhs);
 	float GetLength();
-	Vector2D& GetNormal();
+	Vector2D GetNormal();
 private:
 	float x;
 	float y;
